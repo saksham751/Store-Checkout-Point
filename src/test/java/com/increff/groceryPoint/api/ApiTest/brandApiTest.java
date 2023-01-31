d4f07c7f30a45c711e70dbb5a2e662b8b01600e9
saksham <saksham.yadav@increff.com>
2023-01-31 11:30:28 +0530

    increased coverage

--- src/test/java/com/increff/groceryPoint/api/ApiTest/brandApiTest.java
+++ src/test/java/com/increff/groceryPoint/api/ApiTest/brandApiTest.java
@@ -7,8 +7,12 @@ import com.increff.groceryPoint.model.BrandMasterData;
 import com.increff.groceryPoint.model.BrandMasterForm;
 import com.increff.groceryPoint.pojo.BrandMasterPojo;
 import io.swagger.annotations.Api;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
@@ -16,6 +20,23 @@ public class brandApiTest extends AbstractUnitTest {
     @Autowired
     private BrandMasterApi brandApi;
 
+    @Before
+    public void addSomeBrandData() throws ApiException{
+        BrandMasterPojo brandPojo = new BrandMasterPojo();
+        BrandMasterPojo brandPojo2 = new BrandMasterPojo();
+        BrandMasterPojo brandPojo3 = new BrandMasterPojo();
+        brandPojo.setBrand("brandTest");
+        brandPojo.setCategory("testCategory");
+        brandApi.add(brandPojo);
+
+        brandPojo2.setBrand("brandTest");
+        brandPojo2.setCategory("testCategory2");
+        brandApi.add(brandPojo2);
+
+        brandPojo3.setBrand("Test");
+        brandPojo3.setCategory("testCategory");
+        brandApi.add(brandPojo3);
+    }
     @Test
     public void addTest() throws ApiException {
         BrandMasterPojo brandPojo = new BrandMasterPojo();
@@ -61,4 +82,46 @@ public class brandApiTest extends AbstractUnitTest {
         assertEquals("testbrand",brandPojo2.getBrand());
         assertEquals("testcategory",brandPojo2.getCategory());
     }
+    @Test
+    public void getByBrandTest() throws ApiException{
+        List<BrandMasterPojo> brandList= Arrays.asList(
+                new BrandMasterPojo(),
+                new BrandMasterPojo()
+        );
+        brandList.get(0).setBrand("brandTest");
+        brandList.get(0).setCategory("testCategory");
+        brandList.get(1).setBrand("brandTest");
+        brandList.get(1).setCategory("testCategory2");
+        List<BrandMasterPojo> brandData=brandApi.getByBrand("brandTest");
+        assertEquals(brandList.get(0).getBrand(),brandData.get(0).getBrand());
+        assertEquals(brandList.get(0).getCategory(),brandData.get(0).getCategory());
+        assertEquals(brandList.get(1).getBrand(),brandData.get(1).getBrand());
+        assertEquals(brandList.get(1).getCategory(),brandData.get(1).getCategory());
+    }
+    @Test
+    public void getByCategoryTest() throws ApiException{
+        List<BrandMasterPojo> brandList= Arrays.asList(
+                new BrandMasterPojo(),
+                new BrandMasterPojo()
+        );
+        brandList.get(0).setBrand("brandTest");
+        brandList.get(0).setCategory("testCategory");
+        brandList.get(1).setBrand("Test");
+        brandList.get(1).setCategory("testCategory");
+        List<BrandMasterPojo> brandData=brandApi.getByCategory("testCategory");
+        assertEquals(brandList.get(0).getBrand(),brandData.get(0).getBrand());
+        assertEquals(brandList.get(0).getCategory(),brandData.get(0).getCategory());
+        assertEquals(brandList.get(1).getBrand(),brandData.get(1).getBrand());
+        assertEquals(brandList.get(1).getCategory(),brandData.get(1).getCategory());
+    }
+    @Test
+    public void getByBrandCategoryTest() throws ApiException{
+        BrandMasterPojo brandPojo = new BrandMasterPojo();
+        brandPojo.setBrand("brandTest");
+        brandPojo.setCategory("testCategory");
+        BrandMasterPojo brandData=brandApi.getByBrandCategory("brandTest","testCategory");
+        assertEquals("brandTest",brandData.getBrand());
+        assertEquals("testCategory",brandData.getCategory());
+    }
 }
+
